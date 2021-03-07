76cb1cfa84557ea66db85178aa8202b968e92404
sudarakas <sudharakafb@gmail.com>
2021-03-07 19:08:43 +0530

    added direbase and build PWA

--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -3,7 +3,7 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-square',
   template: `
-    <button nbButton *ngIf="!value">{{ value }}</button>
+    <button nbButton hero status="basic" *ngIf="!value">{{ value }}</button>
     <button nbButton hero status="success" *ngIf="value == 'X'">{{ value }}</button>
     <button nbButton hero status="primary" *ngIf="value == 'O'">{{ value }}</button>
   `,
